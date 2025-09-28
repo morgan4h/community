09df9e59feb13840b0c14b8864b29fd2757cd8f6
morgan4h <lagosmorgan0@gmail.com>
2025-09-28 18:27:58 -0400

    add the routing for the web app

---
+++ js/app.js
@@ -0,0 +1,31 @@
+console.log("hello world");
+
+// routing section
+
+function redirect(dataValue) {
+    // console.log(dataValue);
+    if(dataValue == 1) {
+        console.log('go to download page')
+        location.href = '../public/download.html'
+    }else if (dataValue == 2) {
+        console.log('go to login page')
+        location.href = '../public/login.html'
+    }else if (dataValue == 3) {
+        console.log('go to upload page')
+        location.href = '../public/upload.html'
+    }else if (dataValue == 4) {
+        console.log('www.youtube.com/@SOFAI4H')
+        window.open('https://www.youtube.com/@SOFAI4H')
+    } else {
+        console.log('there is no page to go to')
+    }
+    return dataValue;
+}
+
+
+// create the event function that get the destination 
+document.addEventListener('click', function(e) {
+    let destination = e.target.getAttribute('data-route');
+    redirect(destination)
+})
+
