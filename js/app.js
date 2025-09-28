61a83806436631c5b538af20b845a1375094641d
morgan4h <lagosmorgan0@gmail.com>
2025-09-28 18:31:32 -0400

    fix the path with adding community repo to the link

--- js/app.js
+++ js/app.js
@@ -6,13 +6,13 @@ function redirect(dataValue) {
     // console.log(dataValue);
     if(dataValue == 1) {
         console.log('go to download page')
-        location.href = '../public/download.html'
+        location.href = '../community/public/download.html'
     }else if (dataValue == 2) {
         console.log('go to login page')
-        location.href = '../public/login.html'
+        location.href = '../community/public/login.html'
     }else if (dataValue == 3) {
         console.log('go to upload page')
-        location.href = '../public/upload.html'
+        location.href = '../community/public/upload.html'
     }else if (dataValue == 4) {
         console.log('www.youtube.com/@SOFAI4H')
         window.open('https://www.youtube.com/@SOFAI4H')
